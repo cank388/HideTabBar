6b5ad02ccbc85d247d85e75e456c38715eaf0acb
Can <can.kalender@mobuy.com.tr>
2019-08-27 14:20:41 +0300

    Done

---
+++ HideTabBar/SecondViewController.swift
@@ -0,0 +1,17 @@
+
+//
+//  SecondViewController.swift
+//  HideTabBar
+//
+//  Created by Can Kalender on 27.08.2019.
+//  Copyright © 2019 Can Kalender. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "SecondVC"
+    }
+}
--- HideTabBar/ViewController.swift
+++ HideTabBar/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClickButton(_ sender: UIButton) {
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
 }
 
